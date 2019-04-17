6a3f475cbbd529c1947c0d6cca7e52df98fe5884
Thomas Forgione <thomas@forgione.fr>
2019-04-17 22:02:37 +0200

    Clean PartialEq

--- src/units.rs
+++ src/units.rs
@@ -4,6 +4,7 @@
 /// The main conversion rules used so far are that 1 in = 72.27 pt = 2.54 cm and 1 pt = 65,536 sp.
 use std::f64;
 use std::fmt;
+use std::ops::Add;
 
 use serde::{Deserialize, Serialize};
 
@@ -20,15 +21,15 @@ pub const MINUS_INFINITY: Sp = Sp(-10_000_000_000);
 /// integer arithmetics by treating all dimensions as integer multiples of
 /// this tiny unit. This ensures consistent computations, and thus output,
 /// across a wide variety of computers.
-#[derive(Copy, Clone, Serialize, Deserialize)]
+#[derive(Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Sp(pub i64);
 
 /// Millimeters.
-#[derive(Copy, Clone, Serialize, Deserialize)]
+#[derive(Copy, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Mm(pub f64);
 
 /// Points.
-#[derive(Copy, Clone, Serialize, Deserialize)]
+#[derive(Copy, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Pt(pub f64);
 
 impl fmt::Debug for Sp {
@@ -49,6 +50,22 @@ impl fmt::Debug for Pt {
     }
 }
 
+macro_rules! impl_add {
+    ($the_type: ty, $constructor: expr) => {
+        impl Add for $the_type {
+            type Output = $the_type;
+
+            fn add(self, other: $the_type) -> $the_type {
+                $constructor(self.0 + other.0)
+            }
+        }
+    }
+}
+
+impl_add!(Sp, Sp);
+impl_add!(Mm, Mm);
+impl_add!(Pt, Pt);
+
 impl From<Mm> for Sp {
     fn from(mm: Mm) -> Sp {
         // 10 mm = 1 cm
@@ -59,12 +76,6 @@ impl From<Mm> for Sp {
 impl Into<Mm> for Sp {
     fn into(self) -> Mm {
         Mm((25.4 / (72.27 * 65_536.0)) * (self.0 as f64))
-    }
-}
-
-impl PartialEq<Sp> for Sp {
-    fn eq(&self, other: &Sp) -> bool {
-        self.0 == other.0
     }
 }
 
@@ -89,18 +100,6 @@ impl From<Pt> for Mm {
 impl Into<Pt> for Mm {
     fn into(self) -> Pt {
         Pt((72.27 / 25.4) * self.0)
-    }
-}
-
-impl PartialEq<Mm> for Mm {
-    fn eq(&self, other: &Mm) -> bool {
-        nearly_equal(self.0, other.0)
-    }
-}
-
-impl PartialEq<Pt> for Pt {
-    fn eq(&self, other: &Pt) -> bool {
-        nearly_equal(self.0, other.0)
     }
 }
 
@@ -136,7 +135,7 @@ pub fn nearly_equal(a: f64, b: f64) -> bool {
 /// Unit tests for SpanDeX.
 #[cfg(test)]
 mod tests {
-    use crate::units::{Mm, Pt, Sp};
+    use crate::units::{Mm, Pt, Sp, nearly_equal};
 
     #[test]
     fn convert_mm_to_sp() {
@@ -159,7 +158,7 @@ mod tests {
         let expected_result = Mm(17.5);
         let size_in_sp = Sp(3_263_189);
         let cast_from_sp: Mm = size_in_sp.into();
-        assert_eq!(cast_from_sp, expected_result);
+        assert!(nearly_equal(cast_from_sp.0, expected_result.0));
     }
 
     #[test]
@@ -167,7 +166,7 @@ mod tests {
         let expected_result = Pt(20.25);
         let size_in_sp = Sp(1_327_104);
         let cast_from_sp: Pt = size_in_sp.into();
-        assert_eq!(cast_from_sp, expected_result);
+        assert!(nearly_equal(cast_from_sp.0, expected_result.0));
     }
 
     #[test]
@@ -175,7 +174,7 @@ mod tests {
         let expected_result = Mm(4.481);
         let size_in_pt = Pt(12.75);
         let cast_from_pt = Mm::from(size_in_pt);
-        assert_eq!(cast_from_pt, expected_result);
+        assert!(nearly_equal(cast_from_pt.0, expected_result.0));
     }
 
     #[test]
@@ -183,6 +182,6 @@ mod tests {
         let expected_result = Pt(12.75);
         let size_in_mm = Mm(4.481);
         let cast_from_mm: Pt = size_in_mm.into();
-        assert_eq!(cast_from_mm, expected_result);
+        assert!(nearly_equal(cast_from_mm.0, expected_result.0));
     }
 }
