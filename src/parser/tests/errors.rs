b1846ff23e3b1f01cf44cfb44ff8e8022ece6c08
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-01-23 18:30:15 +0100

    Remove position offsets in errors tests
    Offset may be different on Windows and Unix because
    of how new lines are handled.

--- src/parser/tests/errors.rs
+++ src/parser/tests/errors.rs
@@ -24,7 +24,6 @@ fn test_unmatched_star() -> Result<()> {
     assert_eq!(p.ty, ErrorType::UnmatchedStar);
     assert_eq!(p.position.line, 1);
     assert_eq!(p.position.column, 10);
-    assert_eq!(p.position.offset, 9);
     Ok(())
 }
 
@@ -40,7 +39,6 @@ fn test_unmatched_slash() -> Result<()> {
     assert_eq!(p.ty, ErrorType::UnmatchedSlash);
     assert_eq!(p.position.line, 1);
     assert_eq!(p.position.column, 7);
-    assert_eq!(p.position.offset, 6);
     Ok(())
 }
 
@@ -56,7 +54,6 @@ fn test_unmatched_dollar() -> Result<()> {
     assert_eq!(p.ty, ErrorType::UnmatchedDollar);
     assert_eq!(p.position.line, 1);
     assert_eq!(p.position.column, 13);
-    assert_eq!(p.position.offset, 12);
     Ok(())
 }
 
@@ -72,7 +69,6 @@ fn test_mixed_star_slash() -> Result<()> {
     assert_eq!(p.ty, ErrorType::UnmatchedStar);
     assert_eq!(p.position.line, 1);
     assert_eq!(p.position.column, 21);
-    assert_eq!(p.position.offset, 20);
 
     Ok(())
 }
@@ -91,7 +87,6 @@ fn test_title_no_new_line() -> Result<()> {
     assert_eq!(p.ty, ErrorType::MultipleLinesTitle);
     assert_eq!(p.position.line, 2);
     assert_eq!(p.position.column, 1);
-    assert_eq!(p.position.offset, 18);
 
     Ok(())
 }
@@ -110,7 +105,6 @@ fn test_accent() -> Result<()> {
     assert_eq!(e.ty, ErrorType::UnmatchedStar);
     assert_eq!(e.position.line, 1);
     assert_eq!(e.position.column, 46);
-    assert_eq!(e.position.offset, 46);
 
     Ok(())
 }
